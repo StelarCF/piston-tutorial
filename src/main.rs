4378332b86e8c9c942dd4c6bf99043ac188cf9ad
StelarCF <StelarCF@gmail.com>
2015-10-30 08:34:26 +0200

    Part 2: Input

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,82 @@ extern crate piston_window;
 
 use piston_window::*;
 
+struct Game {
+    rotation: f64,
+    x: f64,
+    y: f64,
+    up_d: bool, down_d: bool, left_d: bool, right_d: bool
+}
+
+impl Game {
+    fn new() -> Game {
+        Game { rotation : 0.0, x : 0.0, y : 0.0, up_d: false, down_d: false, left_d: false, right_d: false }
+    }
+    fn on_update(&mut self, upd: UpdateArgs) {
+        self.rotation += 3.0 * upd.dt;
+        if self.up_d {
+            self.y += (-50.0) * upd.dt;
+        }
+        if self.down_d {
+            self.y += (50.0) * upd.dt;
+        }
+        if self.left_d {
+            self.x += (-50.0) * upd.dt;
+        }
+        if self.right_d {
+            self.x += (50.0) * upd.dt;
+        }
+    }
+    fn on_draw(&mut self, ren: RenderArgs, e: PistonWindow) {
+        e.draw_2d(|c, g| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            let center = c.transform.trans((ren.width / 2) as f64, (ren.height / 2) as f64);
+            let square = rectangle::square(0.0, 0.0, 100.0);
+            let red = [1.0, 0.0, 0.0, 1.0];
+            rectangle(red, square, center.trans(self.x, self.y).rot_rad(self.rotation).trans(-50.0, -50.0), g); // We translate the rectangle slightly so that it's centered; otherwise only the top left corner would be centered
+        });
+    }
+    fn on_input(&mut self, inp: Input) {
+        match inp {
+            Input::Press(but) => {
+                match but {
+                    Button::Keyboard(Key::Up) => {
+                        self.up_d = true;
+                    }
+                    Button::Keyboard(Key::Down) => {
+                        self.down_d = true;
+                    }
+                    Button::Keyboard(Key::Left) => {
+                        self.left_d = true;
+                    }
+                    Button::Keyboard(Key::Right) => {
+                        self.right_d = true;
+                    }
+                    _ => {}
+                }
+            }
+            Input::Release(but) => {
+                match but {
+                    Button::Keyboard(Key::Up) => {
+                        self.up_d = false;
+                    }
+                    Button::Keyboard(Key::Down) => {
+                        self.down_d = false;
+                    }
+                    Button::Keyboard(Key::Left) => {
+                        self.left_d = false;
+                    }
+                    Button::Keyboard(Key::Right) => {
+                        self.right_d = false;
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
     let window: PistonWindow = WindowSettings::new(
         "piston-tutorial",
@@ -10,22 +86,21 @@ fn main() {
     .exit_on_esc(true)
     .build()
     .unwrap();
-    let mut rotation: f64 = 0.0;
+    let mut game = Game::new();
     for e in window {
         match e.event {
-            Some(Event::Update(UpdateArgs { dt })) => {
-                rotation += 3.0 * dt;
+            Some(Event::Update(upd)) => {
+                game.on_update(upd);
+            }
+            Some(Event::Render(ren)) => {
+                game.on_draw(ren, e);
+            }
+            Some(Event::Input(inp)) => {
+                game.on_input(inp);
             }
             _ => {
 
             }
         }
-        e.draw_2d(|c, g| {
-            clear([0.0, 0.0, 0.0, 1.0], g);
-            let center = c.transform.trans(300.0, 300.0);
-            let square = rectangle::square(0.0, 0.0, 100.0);
-            let red = [1.0, 0.0, 0.0, 1.0];
-            rectangle(red, square, center.rot_rad(rotation).trans(-50.0, -50.0), g); // We translate the rectangle slightly so that it's centered; otherwise only the top left corner would be centered
-        });
     }
 }
