cbf589d106b61fe051b5fa8ce546b82027893a69
StelarCF <StelarCF@gmail.com>
2015-11-08 17:10:31 +0200

    Part 4 Mouse and some improvements

--- src/main.rs
+++ src/main.rs
@@ -10,45 +10,59 @@ mod object;
 use object::Object;
 
 struct Game {
-    rotation: f64,
     player: Object,
-    up_d: bool, down_d: bool, left_d: bool, right_d: bool
+    up_d: bool, down_d: bool, left_d: bool, right_d: bool,
+    scx: f64, scy: f64
 }
 
 impl Game {
     fn new() -> Game {
-        Game { rotation : 0.0, player : Object::new(), up_d: false, down_d: false, left_d: false, right_d: false }
+        Game { player : Object::new(), up_d: false, down_d: false, left_d: false, right_d: false, scx: 300.0, scy: 300.0 }
     }
     fn on_load(&mut self, w: &PistonWindow) {
         let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
-        let tank_sprite = assets.join("E-100_preview.png");
+        let tank_sprite = assets.join("E-100_Base.png");
         let tank_sprite = Texture::from_path(
                 &mut *w.factory.borrow_mut(),
                 &tank_sprite,
                 Flip::None,
                 &TextureSettings::new())
                 .unwrap();
-        self.player.setSprite(tank_sprite);
+        let tank_turret = assets.join("E-100_Turret.png");
+        let tank_turret = Texture::from_path(
+                &mut *w.factory.borrow_mut(),
+                &tank_turret,
+                Flip::None,
+                &TextureSettings::new())
+                .unwrap();
+        self.player.set_sprite(tank_sprite);
+        self.player.set_turret_sprite(tank_turret);
     }
     fn on_update(&mut self, upd: UpdateArgs) {
-        self.rotation += 3.0 * upd.dt;
         if self.up_d {
-            self.player.mov(0.0, -150.0 * upd.dt);
+            //self.player.mov(0.0, -150.0 * upd.dt);
+            self.player.fwd(150.0 * upd.dt);
         }
         if self.down_d {
-            self.player.mov(0.0, 150.0 * upd.dt);
+            //self.player.mov(0.0, 150.0 * upd.dt);
+            self.player.fwd(-150.0 * upd.dt);
         }
         if self.left_d {
-            self.player.mov(-150.0 * upd.dt, 0.0);
+            //self.player.mov(-150.0 * upd.dt, 0.0);
+            self.player.rot(-1.0 * upd.dt);
         }
         if self.right_d {
-            self.player.mov(150.0 * upd.dt, 0.0);
+            //self.player.mov(150.0 * upd.dt, 0.0);
+            self.player.rot(1.0 * upd.dt);
         }
+        self.player.update(upd.dt);
     }
     fn on_draw(&mut self, ren: RenderArgs, e: PistonWindow) {
+        self.scx = (ren.width / 2) as f64;
+        self.scy = (ren.height / 2) as f64;
         e.draw_2d(|c, g| {
             clear([0.8, 0.8, 0.8, 1.0], g);
-            let center = c.transform.trans((ren.width / 2) as f64, (ren.height / 2) as f64);
+            let center = c.transform.trans(self.scx, self.scy);
             self.player.render(g, center);
         });
     }
@@ -84,6 +98,14 @@ impl Game {
                     }
                     Button::Keyboard(Key::Right) => {
                         self.right_d = false;
+                    }
+                    _ => {}
+                }
+            }
+            Input::Move(mot) => {
+                match mot {
+                    Motion::MouseCursor(x, y) => {
+                        self.player.point_tur_to(x - self.scx, y - self.scy);
                     }
                     _ => {}
                 }
--- src/object.rs
+++ src/object.rs
@@ -1,17 +1,23 @@
 use piston_window::*;
 use gfx_device_gl::{Resources, Output, CommandBuffer};
 use gfx_graphics::GfxGraphics;
+use std::f64::consts::PI;
 
 pub struct Object {
     x: f64,
     y: f64,
-    sprite: Option<Texture<Resources>>
+    rot: f64,
+    sprite: Option<Texture<Resources>>,
+    tur_x: f64,
+    tur_y: f64,
+    rot_tur: f64,
+    turret: Option<Texture<Resources>>
 }
 
 #[allow(dead_code)]
 impl Object {
     pub fn new() -> Object {
-        Object {x : 0.0, y : 0.0, sprite: None}
+        Object {x : 0.0, y : 0.0, rot: 0.0, rot_tur: 0.0, tur_x: 0.0, tur_y: 0.0, sprite: None, turret: None}
     }
     pub fn mov(&mut self, x: f64, y: f64) {
         self.x += x;
@@ -21,19 +27,81 @@ impl Object {
         self.x = x;
         self.y = y;
     }
+    pub fn rot(&mut self, r: f64) {
+        self.rot += r;
+    }
+    pub fn rot_to(&mut self, r: f64) {
+        self.rot = r;
+    }
+    pub fn fwd(&mut self, d: f64) {
+        self.x += d * (-self.rot.sin());
+        self.y += d * self.rot.cos();
+    }
+    pub fn point_tur_to(&mut self, x: f64, y: f64) {
+        self.tur_x = x;
+        self.tur_y = y;
+    }
+    pub fn calc_tur_pos(&mut self, dt: f64) {
+        let mut new_rot = (-(self.tur_x - self.x)).atan2(self.tur_y - self.y);
+        if new_rot == self.rot_tur {
+            return;
+        }
+        if new_rot < self.rot_tur && self.rot_tur - new_rot > new_rot + 2.0 * PI - self.rot_tur {
+            new_rot += 2.0 * PI;
+        }
+        if new_rot > self.rot_tur && new_rot - self.rot_tur > self.rot_tur + 2.0 * PI - new_rot {
+            new_rot -= 2.0 * PI;
+        }
+        let rot_speed = 1.0;
+        if new_rot > self.rot_tur {
+            if new_rot - self.rot_tur > rot_speed * dt {
+                self.rot_tur += rot_speed * dt;
+            } else {
+                self.rot_tur = new_rot;
+            }
+        } else {
+            if self.rot_tur - new_rot > rot_speed * dt {
+                self.rot_tur -= rot_speed * dt;
+            } else {
+                self.rot_tur = new_rot;
+            }
+        }
+        if self.rot_tur > 2.0 * PI {
+            self.rot_tur -= 2.0 * PI;
+        }
+        if self.rot_tur < 0.0 {
+            self.rot_tur += 2.0 * PI;
+        }
+    }
+    pub fn update(&mut self, dt: f64) {
+        self.calc_tur_pos(dt);
+    }
     pub fn render(&self, g: &mut GfxGraphics<Resources, CommandBuffer<Resources>, Output>, view: math::Matrix2d) {
-        let square = rectangle::square(0.0, 0.0, 100.0);
-        let red = [1.0, 0.0, 0.0, 1.0];
         match self.sprite {
             None => {
+                let square = rectangle::square(0.0, 0.0, 100.0);
+                let red = [1.0, 0.0, 0.0, 1.0];
                 rectangle(red, square, view.trans(self.x, self.y).trans(-50.0, -50.0), g); // We translate the rectangle slightly so that it's centered; otherwise only the top left corner would be centered
             }
             Some(ref sprite) => {
-                image(sprite, view.trans(self.x, self.y).trans(-50.0, -50.0), g);
+                let (spritex, spritey) = sprite.get_size();
+                let (ocx, ocy) = (spritex / 2, spritey / 2);
+                image(sprite, view.trans(self.x, self.y).scale(0.75, 0.75).rot_rad(self.rot).trans(-(ocx as f64), -(ocy as f64)), g);
+            }
+        }
+        match self.turret {
+            None => {}
+            Some(ref sprite) => {
+                let (spritex, spritey) = sprite.get_size();
+                let (ocx, ocy) = (spritex / 2, spritey / 2);
+                image(sprite, view.trans(self.x, self.y).scale(0.75, 0.75).rot_rad(self.rot_tur).trans(-(ocx as f64), -(ocy as f64)), g);
             }
         }
     }
-    pub fn setSprite(&mut self, sprite: Texture<Resources>) {
+    pub fn set_sprite(&mut self, sprite: Texture<Resources>) {
         self.sprite = Some(sprite);
+    }
+    pub fn set_turret_sprite(&mut self, sprite: Texture<Resources>) {
+        self.turret = Some(sprite);
     }
 }
